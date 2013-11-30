06f2305ea827d489d1b29f2838f124f5ae8de2c7
Ryan Koven <celeritas@ucla.edu>
2013-11-30 00:25:08 -0800

    Add ex2-7.c

---
+++ src/chapter2/ex2-7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int invert(const unsigned int, const int, const int);
+
+int main(int argc, char *argv[]){
+	if (argc < 4){
+		printf("Usage %s <int to invert bits on> <start pos> <number of bits>\n", argv[0]);
+		exit(1);
+	}
+	printf("%d\n", invert(atoi(argv[1]), atoi(argv[2]), atoi(argv[3])));
+	return 0;
+}
+
+/* invert: returns x with the n bits that begin at position p
+   inverted (i.e., 1 changed to 0 and vice verse), with others
+   unchanged.  */
+unsigned int invert(const unsigned int x, const int p, const int n){
+	unsigned kill_mask; 
+	kill_mask = 0 | (2*(n - 1) + 1);
+						
+	return (x ^ (kill_mask << (p - n + 1)));	                     
+}
