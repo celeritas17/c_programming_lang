c059af92f8388b087fbb4a2a57f0347bdca6343b
Ryan Koven <celeritas@ucla.edu>
2013-11-30 01:42:03 -0800

    Refactor ex2-6.c

--- src/chapter2/ex2-6.c
+++ src/chapter2/ex2-6.c
@@ -25,16 +25,10 @@ unsigned int setbits(const unsigned int x, const int p, const int n, const unsig
 	unsigned int i, new_bits, kill_mask = ~0; 
 	new_bits = getbits(y, n - 1, n); // get bits to set from y.
 	
-	for (i = 0; i < p - n + 1; i++)
-		new_bits = new_bits << 1; // shift new bits into correct position.
-														
-	for (i = 0; i < n; i++)
-		kill_mask = (kill_mask << 1); // add 0s for n the bits to kill in x.
+	new_bits = new_bits << (p - n + 1); // shift new bits into correct position.
 	
-	for (i = 0; i < p - n + 1; i++)
-		kill_mask = (kill_mask << 1) | 1; // kill_mask will have 0s for the bits 
-						  // to kill in x, 1s everywhere else.
-	
+	kill_mask = (kill_mask << (p + 1)) | ((p > 0) ? (2*(p - n) + 1) : 0); // kill_mask will have 0s for the bits 
+						            // to kill in x, 1s everywhere else.
 	return (x & kill_mask) | new_bits; // & kills the bits to replace in x;
 	                                   // | sets bits from new_bits
 }
