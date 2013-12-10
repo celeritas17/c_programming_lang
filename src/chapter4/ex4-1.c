5e4de09fcf5022e864d892b6d85b0b9773494d90
Ryan Koven <celeritas@ucla.edu>
2013-12-09 21:57:54 -0800

    Cleaning up ex4-1.c

--- src/chapter4/ex4-1.c
+++ src/chapter4/ex4-1.c
@@ -18,9 +18,9 @@ int strrindex(char s[], char t[]){
 	int i, j, k;
 	
 	for (i = strlen(s) - 1; i >= 0; i--){
-		for (j = i, k = 0; t[k] != '\0' && s[j] == t[k] ;j++, k++)
+		for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
 			;
-		if (k > 0 &&  t[k] == '\0')
+		if (k > 0 && t[k] == '\0')
 			return i;	
 	}
 	return -1;
