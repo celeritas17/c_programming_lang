6b081a14205d81b4ae106bcf7fc78dbf3031236e
Ryan Koven <celeritas@ucla.edu>
2013-12-10 23:41:18 -0800

    Add atof from ex4-2.c to c_test.h

--- test/c_test.h
+++ test/c_test.h
@@ -331,4 +331,37 @@ void shellsort(int v[], int n){
 	}
 }
 
+/* atof: convert string s to double; accepts strings
+   representing digits in scientific notation. */
+double __atof(char s[]){
+	double val, power, e_power;
+	int i, j, exponent, sign, e_sign;
+
+	for (i = 0; isspace(s[i]); i++) /* skip white space */
+		;
+	sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	for (val = 0.0; isdigit(s[i]); i++)
+		val = 10.0*val + (s[i] - '0');
+	if (s[i] =='.')
+		i++;
+	for (power = 1.0; isdigit(s[i]); i++){
+		val = 10.0*val + (s[i] - '0');
+		power *= 10.0;
+	}
+	
+	if (s[i] == 'e' || s[i] == 'E')
+		i++;
+	e_sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	for (exponent = 0; isdigit(s[i]); i++)
+		exponent = exponent*10 + (s[i] - '0');
+	for (j = 0, e_power = 1.0; j < exponent; j++)
+		e_power *= (e_sign == 1) ? 10 : .1;
+
+	return sign*val*e_power/power;
+}
+
 #endif
