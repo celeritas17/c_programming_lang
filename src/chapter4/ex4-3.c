4b9437159243537ccb35c2b699787ff3d5d88a70
Ryan Koven <celeritas@ucla.edu>
2013-12-11 19:29:19 -0800

    Add values global variables and functions for stack in ex4-3.c

--- src/chapter4/ex4-3.c
+++ src/chapter4/ex4-3.c
@@ -46,3 +46,26 @@ main(){
 	}
 	return 0;
 }
+
+#define MAXVAL 100   /* maximum depth of val stack */
+
+int sp = 0;          /* maximum free stack position */
+double val[MAXVAL];  /* value stack */
+
+/* push: push f onto value stack */
+void push(double f){
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void){
+	if (sp > 0)
+		return val[--sp];
+	else{
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
