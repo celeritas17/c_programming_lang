814cb42dfac694da00f730ee1b86f9908524e1b4
Ryan Koven <celeritas@ucla.edu>
2013-12-11 20:32:16 -0800

    Add getch, ungetch, associated global variables

--- src/chapter4/ex4-3.c
+++ src/chapter4/ex4-3.c
@@ -96,3 +96,19 @@ int getop(char s[]){
 		ungetch(c);
 	return NUMBER;
 }
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];  /* buffer for ungetch */
+int bufp = 0;       /* next free position in buf */
+
+int getch(void){ /* get a (possible pushed back) character */
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){ /*push character back on input */
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
