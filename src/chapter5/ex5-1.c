012cdf479887d3913cac6d7541992d76972c1b18
Ryan Koven <celeritas@ucla.edu>
2013-12-21 22:52:09 -0800

    Start ex5-1.c

---
+++ src/chapter5/ex5-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/*getint: get next integer from input into *pn */
+int getint(int *pn){
+	int c, sign;
+
+	return c;
+}
