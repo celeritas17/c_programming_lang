715eea9d12ae0ef235e86e144c0086c98e6c2970
Ryan Koven <celeritas@ucla.edu>
2013-12-27 14:12:57 -0800

    Add __strncpy

--- src/chapter5/ex5-5.c
+++ src/chapter5/ex5-5.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void strncpy(char *, char *, int);
-void strncat(char *, char *, int);
-int strncmp(char *, char *, int);
+void __strncpy(char *, char *, int);
+void __strncat(char *, char *, int);
+int __strncmp(char *, char *, int);
 
 int main(int argc, char *argv[]){
-
 	return 0;
+}
+
+/* __strncpy: copy the first n characters of the string t to s */
+void __strncpy(char *s, char *t, int n){
+	int i;
+	for (i = 0; *t && i < n; s++, t++, i++)
+		*s = *t;
+	*s = '\0';
 }
