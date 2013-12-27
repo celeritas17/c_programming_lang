4aa5866a92fbbcbc6e8499ca1c7b80aa980dd435
Ryan Koven <celeritas@ucla.edu>
2013-12-27 13:59:33 -0800

    Add ex5-5.c

---
+++ src/chapter5/ex5-5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void strncpy(char *, char *, int);
+void strncat(char *, char *, int);
+int strncmp(char *, char *, int);
+
+int main(int argc, char *argv[]){
+
+	return 0;
+}
