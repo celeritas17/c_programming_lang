689d19ef8fa3f4f4138ca3f427b41a28b08c3ea6
Ryan Koven <celeritas@ucla.edu>
2013-12-27 14:51:22 -0800

    Add __strncmp

--- src/chapter5/ex5-5.c
+++ src/chapter5/ex5-5.c
@@ -6,8 +6,6 @@ void __strncat(char *, char *, int);
 int __strncmp(char *, char *, int);
 
 int main(int argc, char *argv[]){
-	//__strncat(argv[1], argv[2], atoi(argv[3]));
-	//printf("%s\n", argv[1]);
 	return 0;
 }
 
@@ -27,4 +25,19 @@ void __strncat(char *s, char *t, int n){
 	for (i = 0; *t && i < n; s++, t++, i++)
 		*s = *t;
 	*s = '\0';
+}
+
+/* __strncmp: compares the first n characters of s with the first n
+  characters of t; returns 1 if they all match or all of t is found 
+  in s, 0 otherwise (the standard library function strncmp returns 
+  different values). */
+int __strncmp(char *s, char *t, int n){
+	int i;
+	
+	for (i = 0; *t && *s && *s == *t && i < n; i++, s++, t++)
+		;
+	return ((i && i == n) || !*t); /* all n chars match
+					  (n > 0) or all of 
+					  t matches 
+  				       */
 }
