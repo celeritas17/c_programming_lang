be581e2d9e6eaf186c19e9bb15cf3f5ea3eb3488
Ryan Koven <celeritas@ucla.edu>
2014-01-02 23:27:08 -0800

    Add is_permutation

--- src/misc/is_permutation.c
+++ src/misc/is_permutation.c
@@ -12,7 +12,22 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-
+/* is_permutation: returns 1 if s is a permutation of t, 0 otherwise; s, t ascii strings. */
 int is_permutation(char *s, char *t){
+	int i, s_chars[127], t_chars[127];
+	char *c;
+	
+	for (i = 0; i < 127; i++)
+		s_chars[i] = t_chars[i] = 0;
+	
+	for (c = s; *c; c++)
+		s_chars[*c]++;
+	for (c = t; *c; c++)
+		t_chars[*c]++;
+
+	for (i = 0; i < 127; i++){
+		if (s_chars[i] != t_chars[i])
+			return 0;
+	}
 	return 1;	
 }
